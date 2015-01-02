5f94f1b97a142286d8fa7cdcef2abd2c24f1e138
조형규 <dreamboy@miraesoft.kr>
2015-01-02 21:53:06 +0900

    groupid 확정. 도메인 확보. 실제 패키지 생성 springboot 동작 시험.

---
+++ src/main/java/org/springapp/sboard/SboardApplication.java
@@ -0,0 +1,17 @@
+package org.springapp.sboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SboardApplication {
+	
+	public static void main(String [] args) {
+		SpringApplication.run(SboardApplication.class, args);
+	}
+
+}
