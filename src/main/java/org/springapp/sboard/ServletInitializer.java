0650a0d2d33efe9d6f09cab470924ae6bf6c88fc
조형규 <dreamboy@miraesoft.kr>
2015-01-09 12:25:20 +0900

    demo 삭제.

--- src/main/java/org/springapp/sboard/ServletInitializer.java
+++ src/main/java/org/springapp/sboard/ServletInitializer.java
@@ -1,4 +1,4 @@
-package demo;
+package org.springapp.sboard;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
